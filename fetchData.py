cbb33c3c887248669442c167d30d2e485a7ffcb4
asakeaoyama <aoyama07240316@gmail.com>
2023-11-13 22:08:47 +0800

    add bbc-news instance for example execution

--- fetchData.py
+++ fetchData.py
@@ -1,23 +1,55 @@
 import requests 
 import json
+import csv
 
-# 超連結
-url = 'https://www.google.com/maps/rpc/listugcposts?authuser=0&hl=zh-TW&gl=tw&pb=!1m7!1s0x346e0f45b2c9a5f7%3A0x83aa3c36d06307!3s!6m4!4m1!1e1!4m1!1e3!2m2!1i10!2sCAESBkVnSUlDZw%3D%3D!3e1!5m2!1sygM1ZYnDAezg2roPxsi_sA0!7e81!8m5!1b1!2b1!3b1!5b1!7b1!11m10!1e3!2e1!3szh-TW!4stw!5m3!1s2024-01-20!3i3!4e3!6m1!1i2'
-# 發送get請求
-text = requests.get(url).text
-# 取代掉特殊字元，這個字元是為了資訊安全而設定的喔。
-pretext = ')]}\''
-text = text.replace(pretext,'')
-# 把字串讀取成json
-soup = json.loads(text)
 
-# 取出包含留言的List 。
-# conlist[[rate, comment]]
-conlist = []
-for i in range(10):
-    conlist.append([soup[2][i][0][2][0][0], soup[2][i][0][2][1][0]])
+urlSet = []
+# 開啟 CSV 檔案
+with open('urlSet.csv', newline='') as csvfile:
 
-# 逐筆抓出
-for i in conlist:
-    print("rate:"+str(i[0]))
-    print("comment:"+str(i[1]))
+    # 讀取 CSV 檔案內容
+    rows = csv.reader(csvfile)
+
+    # 以迴圈輸出每一列
+    for row in rows:
+        if len(row) > 0: urlSet.append(row[0])
+
+c = 0
+for url in urlSet:
+    c += 1
+    print("Line", c)
+    # 發送get請求
+    text = requests.get(url).text
+    # 取代掉特殊字元，這個字元是為了資訊安全而設定的喔。
+    pretext = ')]}\''
+    text = text.replace(pretext,'')
+    # 把字串讀取成json
+    soup = json.loads(text)
+
+    # 取出包含留言的List 。
+    # conlist[[rate, comment]]
+    conlist = []
+    for i in range(10):
+        conlist.append([soup[2][i][0][2][0][0], soup[2][i][0][2][1][0].replace('\n', ' ')])
+
+
+    # 開啟輸出的 CSV 檔案
+    with open('commentData.csv', 'a', newline='') as csvfile:
+        # 建立 CSV 檔寫入器
+        writer = csv.writer(csvfile)
+        # 逐筆抓出
+        for i in conlist:
+            print("rate:", i[0])
+            print("comment:" + str(i[1]))
+
+            # Good Comment
+            if int(i[0]) == 5:
+                # 寫入一列資料
+                writer.writerow(["1", i[1]])
+            # Bad Comment
+            elif int(i[0]) < 4:
+                writer.writerow(["0", i[1]])
+
+
+
+
