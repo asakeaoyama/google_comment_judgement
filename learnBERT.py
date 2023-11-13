f1df9b071802ff63cc9ba8cc851378516ee73982
asakeaoyama <aoyama07240316@gmail.com>
2023-11-13 10:29:39 +0800

    add branch "dev_win" and add prediction function

---
+++ learnBERT.py
@@ -0,0 +1,11 @@
+from transformers import BertTokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+example_text = 'I will watch Memento tonight'
+bert_input = tokenizer(example_text,padding='max_length',
+                       max_length = 10,
+                       truncation=True,
+                       return_tensors="pt")
+# ------- bert_inpu t ------
+print(bert_input['input_ids'])
+print(bert_input['token_type_ids'])
+print(bert_input['attention_mask'])
